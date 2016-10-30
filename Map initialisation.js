7a72b6e9a2198787ca370194cb429e3f146fd221
Utopianii <Isaac.lenny@outlook.com>
2016-10-30 13:22:03 +1100

    Update and rename Map initialisation to Map initialisation.js
    Makes a array that can be treated as a grid

--- Map initialisation.js
+++ Map initialisation.js
@@ -1,8 +1,9 @@
-function(d1max, d2max){
+function(d1max, d2max /*width and length of map*/ ){
 f=new Array();
 for(d1=0; d1<d1max; ++d1){
 f(d1)=new Array();
 for(d2=0; d2<d2max; ++d2)
 {
+//sets each map square to "0"
 f(d1)(d2)=0;
 }}};
